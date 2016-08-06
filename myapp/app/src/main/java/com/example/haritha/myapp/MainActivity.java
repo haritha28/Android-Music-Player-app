26fe3369ad721afdbc4a8a4031621ead5deae81e
haritha28 <haritha.h20@gmail.com>
2016-08-06 23:00:44 +0530

    Seekbar working accordingly with song

--- myapp/app/src/main/java/com/example/haritha/myapp/MainActivity.java
+++ myapp/app/src/main/java/com/example/haritha/myapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.renderscript.Double3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +13,15 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import java.util.concurrent.TimeUnit;
+
 
 
 public class MainActivity extends Activity {
-    private ProgressBar progressBar;
+    //
+    // private ProgressBar progressBar;
     //private Button playButton;
     private MediaPlayer mediaPlayer;
     private Handler handler = new Handler();
@@ -29,6 +34,12 @@ public class MainActivity extends Activity {
     private ImageButton rewind;
     private ImageView nowplay;
     private ImageView namebar;
+    private SeekBar seekBar;
+    private double finaltime;
+    private double startTime;
+    private Handler myHandler = new Handler();
+
+    public static int oneTimeOnly = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +53,7 @@ public class MainActivity extends Activity {
         public void run() {
             double seekPercentage = 100 * mediaPlayer.getCurrentPosition()
                                           /mediaPlayer.getDuration();
-            progressBar.setProgress((int) seekPercentage);
+            //progressBar.setProgress((int) seekPercentage);
             long seconds = (System.currentTimeMillis() - songStartTime) / 1000;
             songtime.setText(String.format("%02d:%02d", seconds/ 60, seconds %60));
             handler.postDelayed(this,1000);
@@ -62,6 +73,8 @@ public class MainActivity extends Activity {
                     handler.removeCallbacks(updateUI);
                     mediaPlayer.pause();
                     playbuttonoriginal.setImageResource(R.drawable.play_button);
+
+
                 }
                 else {
                     initHandler();
@@ -69,32 +82,76 @@ public class MainActivity extends Activity {
                     songStartTime = System.currentTimeMillis();
                     playbuttonoriginal.setImageResource(R.drawable.pause_button);
 
+                    finaltime = mediaPlayer.getDuration();
+                    startTime = mediaPlayer.getCurrentPosition();
+
+                    if (oneTimeOnly == 0) {
+                        seekBar.setMax((int) finaltime);
+                        oneTimeOnly = 1;
+                    }
+
+                    songtime.setText(String.format("%d , %d",
+                            TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                            TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) startTime)))
+                    );
+
+                    seekBar.setProgress((int)startTime);
+                    myHandler.postDelayed(UpdateSongTime, 100);
+
+
                 }
             }
-            }
-        );
+        });
+        
     }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        @Override
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            songtime.setText(String.format("%02d:%02d",TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                    toMinutes((long) startTime)))
+            );
+            seekBar.setProgress((int)startTime);
+            myHandler.postDelayed(this, 100);
+        }
+    };
+
+
 
 //Linking with xml files usnig id
     private void initView() {
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        //progressBar = (ProgressBar) findViewById(R.id.progressBar);
         //playButton = (Button) findViewById(R.id.playbutton);
         mediaPlayer = MediaPlayer.create(this, R.raw.song);
+
         songtime = (TextView) findViewById(R.id.songtime);
         imageview = (ImageView) findViewById(R.id.album_art);
         songtime.setText(String.format("%02d:%02d",0,0));
         imageview.setImageDrawable(getResources().getDrawable(R.drawable.album_art));
+
         playbuttonoriginal = (ImageButton) findViewById(R.id.playbuttonoriginal);
         playbuttonoriginal.setImageDrawable((getResources().getDrawable(R.drawable.play_button)));
+
         //pausebutton = (ImageButton) findViewById(R.id.pausebutton);
         //pausebutton.setImageDrawable((getResources().getDrawable(R.drawable.pause_button)));
+
         nowplay = (ImageView) findViewById(R.id.nowplay);
         nowplay.setImageDrawable(getResources().getDrawable(R.drawable.now_playing));
+
         namebar = (ImageView) findViewById(R.id.namebar);
         namebar.setImageDrawable(getResources().getDrawable(R.drawable.song_title_place_holder));
+
         forward = (ImageButton) findViewById(R.id.forward);
         forward.setImageDrawable(getResources().getDrawable(R.drawable.fast_forward_button));
+
         rewind = (ImageButton) findViewById(R.id.rewind);
         rewind.setImageDrawable(getResources().getDrawable(R.drawable.rewind_button));
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
     }
 }
